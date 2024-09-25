db0aa4d3a5dd7f5fef9dece78ddd12e1b06ad4bc
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-25 22:45:39 +0530

    static

--- 03nexttodos/next.config.mjs
+++ 03nexttodos/next.config.mjs
@@ -1,8 +1,4 @@
-import createJiti from "jiti";
-import { fileURLToPath } from "node:url";
-const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti("./src/env/server.ts")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- 03nexttodos/tailwind.config.ts
+++ 03nexttodos/tailwind.config.ts
@@ -9,14 +9,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
